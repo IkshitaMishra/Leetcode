d55337200088eaefe58dba3a7e88918532998d2a
Ikshita Mishra <iksh.1995@gmail.com>
2018-10-06 22:21:42 -0700

    Add files via upload

---
+++ needleInHaystack.py
@@ -0,0 +1,37 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        
+        if needle in haystack:
+            return haystack.find(needle) 
+        else:
+            return -1
+
+def stringToString(input):
+    import json
+
+    return json.loads(input)
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            haystack = stringToString(line);
+            line = next(lines)
+            needle = stringToString(line);
+            
+            ret = Solution().strStr(haystack, needle)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
---
+++ removeElement.py
@@ -0,0 +1,38 @@
+class Solution:
+    def removeElement(self, nums, val):
+        
+        nums[:]  = [n for n in nums if n != val]
+        return len(nums)
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line);
+            line = next(lines)
+            val = int(line);
+            
+            ret = Solution().removeElement(nums, val)
+
+            out = integerListToString(nums, len_of_list=ret)
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
