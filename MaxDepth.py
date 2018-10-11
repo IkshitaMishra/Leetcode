f27aea3edfa8f06f69651ac53783057c14bf63f6
Ikshita Mishra <iksh.1995@gmail.com>
2018-10-11 15:24:30 -0700

    Tree

---
+++ MaxDepth.py
@@ -0,0 +1,80 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        if root is None:
+            return 0
+        if root:
+            if root.left is None and root.right is None:
+                return 1
+            if root.left is not None or root.right is not None:
+                return ((max(self.maxDepth(root.left),self.maxDepth(root.right)))+1)
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def intToString(input):
+    if input is None:
+        input = 0
+    return str(input)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            root = stringToTreeNode(line)
+            
+            ret = Solution().maxDepth(root)
+
+            out = intToString(ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
